c2db0cdf7b344eb2c268357804e288ba25d85474
Sanchita Gupta <56127516+sanchita1900@users.noreply.github.com>
2021-08-05 23:36:05 +0530

    fixed ui of cards

--- client/src/Components/DoctorDashboard/PatientCard/PatientCard.js
+++ client/src/Components/DoctorDashboard/PatientCard/PatientCard.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { confirmInvitationApi } from "../../../api";
-
+import "./PatientCard.css";
 const PatientCard = (props) => {
-  //   const [invState, setInvState] = useState();
-
   const handleSubmit = async () => {
     await confirmInvitationApi(props.id);
     window.location.reload();
@@ -11,11 +9,36 @@ const PatientCard = (props) => {
 
   return (
     <>
-      <div className="Card">
-        Hi I am {props.name}, email id is {props.email}. I am sufferring from{" "}
-        {props.disease}. Age if {props.age}. Kindly accept my appointment
-        request.
-        <button onClick={handleSubmit}>Accept</button>
+      <div className="mainPatientCardContainer">
+        <div className="imgContainer">
+          <img
+            src="https://img.icons8.com/ios-filled/250/000000/hand-with-a-pill.png"
+            height="150px"
+            width="180px"
+          />
+        </div>
+        <div className="PatientCard">
+          <div className="request">REQUEST</div>
+          <p>
+            {" "}
+            Name:<span className="Patientname">{props.name}</span>
+          </p>
+          <br />
+          <p>
+            {" "}
+            Email Id: <span>{props.email}</span>
+          </p>
+          <br />
+          <p>
+            Suffering From:
+            <span className="PatientDisease">{props.disease}</span>
+          </p>
+          <br />
+
+          <div className="acceptButton">
+            <button onClick={handleSubmit}>Accept</button>
+          </div>
+        </div>
       </div>
     </>
   );
--- client/src/Components/DoctorDashboard/Prescription/PrescriptionPatCard/PrescriptionPatCard.js
+++ client/src/Components/DoctorDashboard/Prescription/PrescriptionPatCard/PrescriptionPatCard.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import "./PrescriptionPatCard.css";
 
 const PrescriptionPatCard = (props) => {
   useEffect(() => {
@@ -7,16 +8,35 @@ const PrescriptionPatCard = (props) => {
   }, []);
   return (
     <>
-      <div className="Card">
-        Name: {props.name}, Email id: {props.email}. Sufferring from:{" "}
-        {props.disease}. Age: {props.age}
-        {props.button ? (
-          <Link to={`/doctor/chat/${props.id}`}>
-            <button>Prescribe</button>
-          </Link>
-        ) : (
-          <></>
-        )}
+      <div className="mainPatientCardContainer">
+        <div className="PatientCard">
+          <p>
+            {" "}
+            Name: <span className="Patientname">{props.name}</span>
+          </p>
+          <p>
+            {" "}
+            Email id: <span>{props.email}</span>
+          </p>
+          <p>
+            {" "}
+            Disease:
+            <span>{props.disease}</span>
+          </p>
+          <p>
+            {" "}
+            Age: <span>{props.age}</span>
+          </p>
+          {props.button ? (
+            <Link to={`/doctor/chat/${props.id}`}>
+              <div className="acceptButton">
+                <button>Prescribe</button>
+              </div>
+            </Link>
+          ) : (
+            <></>
+          )}
+        </div>
       </div>
     </>
   );
